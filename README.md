14691a6ddb04224de2c0356c9192439dc70aba28
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-10-16 19:52:09 +0900

    [delete] swaggo

--- README.md
+++ README.md
@@ -1,22 +1,2 @@
 # kanbanAPI
 kanbanのAPI
-## Swaggerの設定(自分用)
-`gin-swagger`ってのを使った．
-
-`swag`のコマンドインストール
-```
-go get github.com/swaggo/swag/cmd/swag
-```
-これも
-```
-go get -u github.com/swaggo/http-swagger
-```
-`sagger`の生成
-```
-swag init
-```
-
-[ここ](https://github.com/swaggo/gin-swagger)みてやった．
-
-### swaggerを見て見る
-`http://localhost:9000/swagger/index.html`
--- main.go
+++ main.go
@@ -3,29 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/mytheta/kanbanAPI/controller"
-	"github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
-	_ "./docs"
 
 )
 
 
-// @title Swagger Example API
-// @version 1.0
-// @description This is a kanbanAPI
-
-
-
-// @host localhost:9000
-// @BasePath /
 func main() {
 
 	r := gin.New()
 
 	r.Use(gin.Logger())
 
-	// use ginSwagger middleware to
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.GET("/todo/:status", controller.Todo.FindByStatus)
 
 
